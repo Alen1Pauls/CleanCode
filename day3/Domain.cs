a054da05c99998462a5d2562afa9b6ba4a7ddc7f
Alen1Pauls <alenpauls2001@gmail.com>
2025-03-07 12:34:24 +0530

    Add files via upload

---
+++ day3/Domain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hamaraBasket
+{
+    public class DomainFactory
+    {
+        public List<Item> PrepareItemList(string name, int sellin, int quality)
+        {
+            return new List<Item>
+            {
+                new Item { Name = name, SellIn = sellin, Quality = quality },
+            };
+        }
+        public void UpdateQualityHelper(List<Item> Items)
+        {
+            HamaraBasket app = new HamaraBasket(Items);
+            app.UpdateQuality();
+        }
+
+    }
+
+}
